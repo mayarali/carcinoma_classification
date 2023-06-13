fe94e4f86b16e1bfae6052215f6df0c53b868050
kkontras <konstantinoskontras@gmail.com>
2023-06-13 23:50:27 +0200

    Equalize kfolds

--- main_supervised_kfold.py
+++ main_supervised_kfold.py
@@ -10,6 +10,9 @@ def mean_results(aggregate_results):
     for img in aggregate_results[0]:
         total_results[img] = np.zeros(3)
         for cls in aggregate_results:
+            if (aggregate_results[cls][img] < 0 ).any():
+                aggregate_results[cls][img] = aggregate_results[cls][img] + aggregate_results[cls][img].min()
+            aggregate_results[cls][img] = aggregate_results[cls][img] / aggregate_results[cls][img].sum()
             total_results[img] += aggregate_results[cls][img]
 
     return total_results
