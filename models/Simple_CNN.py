952a109d2b20343c54fdb387565889170128cfa3
kkontras <konstantinoskontras@gmail.com>
2023-06-16 17:05:52 +0200

    Return Features

--- models/Simple_CNN.py
+++ models/Simple_CNN.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torchvision.transforms.functional import pad
+from torchvision.models import resnet50, ResNet50_Weights
+
 class Simple_CNN(nn.Module):
     def __init__(self, encs=None, args=None):
         super().__init__()
@@ -12,18 +14,24 @@ class Simple_CNN(nn.Module):
 
         self.cnn = encs[0]
 
+        # self.cnn = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        # self.cnn.requires_grad_(False)
+
         self.fc = nn.Sequential(
+            nn.Dropout(dropout),
             nn.Linear(d_model, fc_inner),
             nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(fc_inner, fc_inner),
-            nn.ReLU(),
-            nn.Dropout(dropout),
+            # nn.Linear(fc_inner, fc_inner),
+            # nn.ReLU(),
+            # nn.Dropout(dropout),
             nn.Linear(fc_inner, num_classes)
         )
 
-    def forward(self, input):
-        return  self.fc(self.cnn(input))
+    def forward(self, input, return_features=False):
+        features = self.cnn(input)
+        if return_features:
+            return self.fc(features), features
+        return  self.fc(features)
 
 class CNN_part(nn.Module):
     def __init__(self, encs=None, args=None):
@@ -46,6 +54,50 @@ class CNN_part(nn.Module):
     def forward(self, input):
         return self.cnn(input)
 
+class Simple_CNN_Pre(nn.Module):
+    def __init__(self, encs, args=None):
+        super().__init__()
+        self.args = args
+        d_model =  args.dmodel#64*8
+        fc_inner = args.fc_inner
+        num_classes = args.num_classes
+        dropout = args.dropout
+
+        self.cnn = encs[0]
+        self.cnn = encs[1]
+
+        # self.cnn = nn.Sequential(
+        #     nn.BatchNorm2d(3),
+        #     nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2, bias=False),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        #     nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1, bias=False),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),
+        #     nn.AvgPool2d(32, stride=16),
+        #     nn.Flatten(start_dim=1),
+        #     nn.Linear(d_model, fc_inner),
+        #     nn.ReLU(),
+        #     nn.Dropout(dropout),
+        #     nn.Linear(fc_inner, fc_inner),
+        #     nn.ReLU(),
+        #     nn.Dropout(dropout),
+        #     nn.Linear(fc_inner, num_classes-1)
+        # )
+
+    def forward(self, input):
+        # results = torch.zeros(input.shape[0], 3).cuda()
+
+        out_full = self.cnn(input)
+        out_full = pad(out_full, padding=(0, 0, 1, 0))
+
+        if (out_full.argmax(dim=1) == 1).sum() > 0:
+            out_cancer = self.cnn_cancer(input[out_full.argmax(dim=1) == 1])
+            out_full[out_full.argmax(dim=1) == 1][:, 1:] = out_cancer
+
+        return  out_full
 class Simple_CNN_2Binary(nn.Module):
     def __init__(self, encs, args=None):
         super().__init__()
